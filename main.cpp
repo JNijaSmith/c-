3571b41f67b6b67c2509f556bc0518a03cc59126
J'Nija Smith <jnija@students.alcorn.edu>
2025-09-01 18:13:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
-
+// Online C++ compiler to run C++ program online
+#include <iostream>
+std
+int main() {
+  // Write C++ code here
+  cout << "Name: J'Nija Smith" 
+cout << "Major: Electro-Mechanical Engineering"
+cout << "College: Alcorn State University"
+return 0;
